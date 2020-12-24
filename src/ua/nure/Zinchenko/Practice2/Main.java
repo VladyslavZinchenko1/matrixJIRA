74ac9e95344175ba80d0fa98d23ca6d5a7e82b24
ZzZpeach <www.soap2000@gmail.com>
2020-12-24 09:27:23 +0200

    MTX-16 Реализовать каталог

--- src/ua/nure/Zinchenko/Practice2/Main.java
+++ src/ua/nure/Zinchenko/Practice2/Main.java
@@ -1,7 +1,29 @@
 package ua.nure.Zinchenko.Practice2;
 
 public class Main {
- 
+    public Main() {
+        super();
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 
     public static void main(String[] args) {
 
