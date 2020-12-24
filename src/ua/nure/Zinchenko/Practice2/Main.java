d1c7eb96147ce6cf2b44bd063c79d70481f90e57
ZzZpeach <www.soap2000@gmail.com>
2020-12-24 09:45:00 +0200

    MTX-20 Реализация личного кабинета

--- src/ua/nure/Zinchenko/Practice2/Main.java
+++ src/ua/nure/Zinchenko/Practice2/Main.java
@@ -10,20 +10,6 @@ public class Main {
         return super.hashCode();
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
-    }
-
-    @Override
-    protected Object clone() throws CloneNotSupportedException {
-        return super.clone();
-    }
-
-    @Override
-    public String toString() {
-        return super.toString();
-    }
 
     public static void main(String[] args) {
 
